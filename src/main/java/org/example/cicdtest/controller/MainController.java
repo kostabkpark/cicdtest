4e418248d28fbfc741e75023eff59e9399ba34c9
kostabkpark <kostabkpark@gmail.com>
2024-11-26 14:43:26 +0900

    controller add

---
+++ src/main/java/org/example/cicdtest/controller/MainController.java
@@ -0,0 +1,12 @@
+package org.example.cicdtest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "This is manual cicd test server.";
+    }
+}
